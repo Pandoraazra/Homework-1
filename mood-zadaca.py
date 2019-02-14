d86e8153c6eb5702242526b859f7c84e5c5fe3a1
Pandoraazra <46790391+Pandoraazra@users.noreply.github.com>
2019-02-14 13:34:33 +0100

    Add files via upload

---
+++ mood-zadaca.py
@@ -0,0 +1,16 @@
+mood = input("What is your mood today? ")
+
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("it will be alright")
+elif mood == "excited":
+    print("I am happy for you")
+elif mood == "relaxed":
+    print("ommmm")
+
+else:
+    print("I don't recognize this mood")
